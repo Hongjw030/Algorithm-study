ddaa17dd385f29852e903037262b3c12ba4f8bdf
Hongjw030 <89698149+Hongjw030@users.noreply.github.com>
2023-10-23 10:53:00 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,30 +1,11 @@
-const enToNum = [
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-];
-
 function solution(s) {
-  while (isNaN(s)) {
-    s = s.replace(enToNum[0], String(0));
-    s = s.replace(enToNum[1], String(1));
-    s = s.replace(enToNum[2], String(2));
-    s = s.replace(enToNum[3], String(3));
-    s = s.replace(enToNum[4], String(4));
-    s = s.replace(enToNum[5], String(5));
-    s = s.replace(enToNum[6], String(6));
-    s = s.replace(enToNum[7], String(7));
-    s = s.replace(enToNum[8], String(8));
-    s = s.replace(enToNum[9], String(9));
-  }
-  return Number(s);
+    
+    const words = ['zero', 'one', 'two' ,'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let answer = s;
+    
+    words.forEach((item, index) => {
+        answer = answer.split(item).join(index); 
+// answer에서 item 요소를 찾아서, index로 변환함 split으로 item 분할 후 join으로 index로 바꿈
+    }) 
+    return Number(answer); // answer가 문자열이라 Number로 형변환
 }
-
-console.log(solution("one4seveneight"));
