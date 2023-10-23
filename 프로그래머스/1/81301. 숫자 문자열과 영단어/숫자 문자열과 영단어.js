4548428223eb2d486fa3d3a4f4aea056cc68d059
Hongjw030 <89698149+Hongjw030@users.noreply.github.com>
2023-10-23 21:54:15 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,11 +1,20 @@
+const enToNum = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
 function solution(s) {
-    
-    const words = ['zero', 'one', 'two' ,'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let answer = s;
-    
-    words.forEach((item, index) => {
-        answer = answer.split(item).join(index); 
-// answer에서 item 요소를 찾아서, index로 변환함 split으로 item 분할 후 join으로 index로 바꿈
-    }) 
-    return Number(answer); // answer가 문자열이라 Number로 형변환
+  for (let i = 0; i < 10; i++) {
+    s = s.replaceAll(enToNum[i], String(i));
+  }
+  return Number(s);
 }
+
