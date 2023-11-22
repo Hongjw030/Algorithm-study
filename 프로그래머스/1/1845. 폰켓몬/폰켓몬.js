50d5b2898dcd0dee6e095c1459e9bdd3d3b499b5
Hongjw030 <89698149+Hongjw030@users.noreply.github.com>
2023-11-22 10:29:30 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,20 +1,5 @@
 function solution(nums) {
-  // 포켓몬들.
-  const obj = {};
-  nums.forEach((el) => {
-    if (el in obj) {
-      obj[el]++;
-    } else {
-      obj[el] = 1;
-    }
-  });
-
+  const pokes = new Set(nums);
   const total = Math.floor(nums.length / 2);
-  // console.log(Object.keys(obj).length);
-  // console.log(obj);
-  if (total < Object.keys(obj).length) {
-    return total;
-  } else {
-    return Object.keys(obj).length;
-  }
+  return pokes.size > total ? total : pokes.size;
 }
