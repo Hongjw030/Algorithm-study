460dcae4a5da97512490666c46f3006e94b930ec
Hongjw030 <89698149+Hongjw030@users.noreply.github.com>
2023-11-29 23:15:33 +0900

    Update 달리기경주.py

--- 달리기경주.py
+++ 달리기경주.py
@@ -3,7 +3,7 @@
 # 해당 원소를 players에서 찾아 앞의 원소와 위치를 바꾸어준다.
 # 따라서 이중 for 문을 사용해야 하며,
 # players는 5만개 이하, callings는 100만개 이하이므로
-# O(N^2) 시간복잡도 최악의 경우
+# O(N^2) 시간복잡도 최악의 경우 
 
 def solution(players, callings):
     for name in callings:
