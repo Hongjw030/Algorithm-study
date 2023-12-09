d0eb65567f3cbe0b8503ccb2f9676c1ace6626a7
Hongjw030 <89698149+Hongjw030@users.noreply.github.com>
2023-12-09 22:35:08 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.js
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -1,22 +1,11 @@
-function solution(num1, num2) {
-  console.log(num1 + num2);
-  console.log(num1 - num2);
-  console.log(num1 * num2);
-  console.log(Math.floor(num1 / num2));
-  console.log(num1 % num2);
-}
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+let num1 = Number(input[0]);
+let num2 = Number(input[1]);
 
-let input = [];
-readline
-  .on("line", function (line) {
-    input.push(line.split(" "));
-  })
-  .on("close", function () {
-    solution(Number(input[0][0]), Number(input[0][1]));
-    process.exit();
-  });
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(Math.floor(num1 / num2));
+console.log(num1 % num2);
