65e794afff67b1d3aab391eba65de4e25d486404
Hongjw030 <89698149+Hongjw030@users.noreply.github.com>
2023-12-09 22:31:33 +0900

    [Bronze V] Title: 사칙연산, Time: 148 ms, Memory: 9728 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,22 @@
+function solution(num1, num2) {
+  console.log(num1 + num2);
+  console.log(num1 - num2);
+  console.log(num1 * num2);
+  console.log(Math.floor(num1 / num2));
+  console.log(num1 % num2);
+}
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+readline
+  .on("line", function (line) {
+    input.push(line.split(" "));
+  })
+  .on("close", function () {
+    solution(Number(input[0][0]), Number(input[0][1]));
+    process.exit();
+  });
